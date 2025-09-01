da9fa576e88fc62c4289c4176c45ac4361a7b2b9
André Kublik <info@estudionet.net>
2025-09-01 22:59:38 +0000

    Error: Invalid src prop (https://imgur.com/WvnaHQP) on `next/image`, hos

--- src/next.config.ts
+++ src/next.config.ts
@@ -44,6 +44,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
