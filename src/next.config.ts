a89ceac3ff940567275149dcafc06b2b87cd18df
André Kublik <info@estudionet.net>
2025-09-01 22:54:46 +0000

    agrega este tambien: https://studio--enlaceimagen.us-central1.hosted.app

---
+++ src/next.config.ts
@@ -0,0 +1,45 @@
+
+import { config } from 'dotenv';
+config();
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'studio--enlaceimagen.us-central1.hosted.app',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
